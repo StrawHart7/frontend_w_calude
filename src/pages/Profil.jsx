db0467903f06981f2ad27c68124f183d90d8814a
StrawHart7 <wisdomagbodeka482@gmail.com>
2026-04-04 21:03:50 +0000

    v1-05

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -37,7 +37,7 @@ function Profil() {
 
   const handleLogout = () => {
     localStorage.removeItem('token')
-    window.location.href = import.meta.env.VITE_APP_URL + '/login' // ✅
+    window.location.href = import.meta.env.VITE_APP_URL ✅
   }
 
   return (
