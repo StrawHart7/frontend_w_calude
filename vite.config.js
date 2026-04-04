f3c1b29f4dde0fd67c1349f17859d0d3f1eb566e
StrawHart7 <wisdomagbodeka482@gmail.com>
2026-04-04 20:01:14 +0000

    v1.02

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'TodoApp',
-        short_name: 'TodoApp',
-        description: 'Gère tes tâches partout',
-        theme_color: '#6c63ff',
-        background_color: '#0f1117',
-        display: 'standalone',
-        icons: [
-          {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: '/apple-touch-icon.png',
-            sizes: '180x180',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
 })
