3e465e600a6ec688226d182e8e9892beb03b9e13
StrawHart <test@gmail.com>
2026-04-30 05:41:30 +0000

    v1.0.15.4

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom'
 
 function PrivateRoute({ children }) {
-  const token = localStorage.getItem('token')
+  const token = localStorage.getItem('accessToken')
   if (!token) return <Navigate to="/login" />
   return children
 }
